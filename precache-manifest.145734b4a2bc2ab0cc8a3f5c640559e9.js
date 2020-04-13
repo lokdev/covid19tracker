487bebf0466d936bd2bdbf37452cb81688754260
Lokesh Dhanasekar <lokdev@gmail.com>
2020-04-13 08:07:43 +0530

    Updates

---
+++ precache-manifest.145734b4a2bc2ab0cc8a3f5c640559e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbe51b670f827ae95358f1b77e93616",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "2274970b831e23450921",
+    "url": "/covid19tracker/static/css/main.bb0785f9.chunk.css"
+  },
+  {
+    "revision": "03950586f1fd9a017125",
+    "url": "/covid19tracker/static/js/2.b756201a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19tracker/static/js/2.b756201a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2274970b831e23450921",
+    "url": "/covid19tracker/static/js/main.f9aefb04.chunk.js"
+  },
+  {
+    "revision": "5f1894a7e941db1d35a6",
+    "url": "/covid19tracker/static/js/runtime-main.cf4a9811.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19tracker/static/media/image.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lokdev/covid19tracker/precache-manifest.46af866d7ac23e11d0b1c2a33d4f28da.js"
+  "/covid19tracker/precache-manifest.145734b4a2bc2ab0cc8a3f5c640559e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lokdev/covid19tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
