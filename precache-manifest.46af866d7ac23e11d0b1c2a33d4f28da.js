7c6b0162554cc0619376270c47914a2c2715df37
Lokesh Dhanasekar <lokdev@gmail.com>
2020-04-13 08:00:52 +0530

    Updates

---
+++ precache-manifest.46af866d7ac23e11d0b1c2a33d4f28da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3492ae7e36158d2a6826391702ddec1f",
+    "url": "/lokdev/covid19tracker/index.html"
+  },
+  {
+    "revision": "2274970b831e23450921",
+    "url": "/lokdev/covid19tracker/static/css/main.bb0785f9.chunk.css"
+  },
+  {
+    "revision": "74aefce8fbc8222710b9",
+    "url": "/lokdev/covid19tracker/static/js/2.16e0d34f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/lokdev/covid19tracker/static/js/2.16e0d34f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2274970b831e23450921",
+    "url": "/lokdev/covid19tracker/static/js/main.f9aefb04.chunk.js"
+  },
+  {
+    "revision": "943de40fede517ea51b4",
+    "url": "/lokdev/covid19tracker/static/js/runtime-main.09d3b684.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/lokdev/covid19tracker/static/media/image.d7265326.png"
+  }
+]);
